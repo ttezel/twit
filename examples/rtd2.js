925cc75e6d76d4911ab0dc57b6516de11caacec3
Tolga Tezel <tolgatezel11@gmail.com>
2011-11-30 13:57:02 -0500

    updated rtd2 with new api

--- examples/rtd2.js
+++ examples/rtd2.js
@@ -1,8 +1,13 @@
 //
 //  RTD2 - Twitter bot that tweets today's most popular github project
 //
-var config = require('../examples/config')
-  , Bot = require('./bot').Bot
+var config = require('./config')
+  , Twitter = require('../lib/twitter').Twitter;
+
+var randIndex = function (arr) {
+  var index = Math.floor(arr.length*Math.random());
+  return arr[index];
+}
 
 function datestring (d) {
   return d.getUTCFullYear()   + '-' 
@@ -10,39 +15,37 @@ function datestring (d) {
      +   d.getDate();
 }
 
-var Rtd2 = function(config, tracking) {
-  this.bot = new Bot(config);
+var Bot = function(config, tracking) {
+  this.twitter = new Twitter(config);
   this.tracking = tracking;
 };
 
 //
 //  grab today's tweets containing the tracking term
 //
-Rtd2.prototype.track = function (callback) {
-  var self = this
-      , d = new Date(Date.now() - 5*60*60000)  //est timezone
+Bot.prototype.track = function (callback) {
+  var d = new Date(Date.now() - 5*60*60000)  //est timezone
       , dateString = datestring(d);   
     
-    console.log('getting today\'s tweets filtered by `%s`', this.tracking);     
-
-    var path =  '/search.json?q='     + this.tracking +
-                '%20since:'           + dateString    +
-                '&result_type=mixed';
-
-    this.bot.twitter.API('GET', path, {}, function(err, reply) {
-      if(err) return callback(err);
-
-      var tweets = JSON.parse(reply).results
-        , popular = self.getPopular(tweets);
-
-      return callback(null, tweets, popular);
-    });
+  console.log('getting today\'s tweets filtered by `%s`', this.tracking);
+  
+  var params = {
+      q : this.tracking
+    , since: dateString
+    , result_type: 'mixed'
+  };
+  
+  this
+    .twitter
+    .get('search.json')
+    .params(params)
+    .end(callback);
 };
 
 //
 //  get the most popular tweet (by retweet count)
 //
-Rtd2.prototype.getPopular = function (tweets) {
+Bot.prototype.getPopular = function (tweets) {
   var i = tweets.length
     , max = 0
     , popular;
@@ -61,24 +64,99 @@ Rtd2.prototype.getPopular = function (tweets) {
 };
 
 //
-//  Track tweets and tweet the most popular of them
+//  choose a random friend of one of your followers, and follow that user
 //
-Rtd2.prototype.run = function () {
+Bot.prototype.mingle = function (callback) {
   var self = this;
-
+  
   this
+    .twitter
+    .get('followers/ids.json')
+    .end(function(err, reply) {
+      if(err) { return callback(err); }
+      
+      var followers = JSON.parse(reply).ids
+        , randFollower  = randIndex(followers);
+        
+      self
+        .twitter
+        .get('friends/ids.json')
+        .params({ user_id: randFollower })
+        .end(function(err, reply) {
+          if(err) { return callback(err); }
+          
+          var friends = JSON.parse(reply).ids
+            , target  = randIndex(friends);
+            
+          self
+            .twitter
+            .post('friendships/create.json') 
+            .params({ id: target })
+            .end(callback); 
+        })
+    })
 };
 
-var RTD2 = new Rtd2(config, 'github.com/');
-
-RTD2.track(function(err, tweets, popular) {
-  if (err) throw err;
+//
+//  prune your followers list ; unfollow a friend that hasn't followed you back
+//
+Bot.prototype.prune = function (callback) {
+  var self = this;
   
-  console.log('tweeting:', popular);
-
-  var path = 'statuses/update.json?status=' + encodeURIComponent(popular);
-
-  RTD2.bot.twitter.API('POST', path, {}, function(err, reply) {
-    if(err) { console.log('error:', err); }
+  this
+    .twitter
+    .get('followers/ids.json')
+    .end(function(err, reply) {
+      var followers = JSON.parse(reply).ids;
+      
+      self
+        .twitter
+        .get('friends/ids.json')
+        .end(function(err, reply) {
+          var friends = JSON.parse(reply).ids
+            , target = randIndex(friends)
+            , pruned = false;
+          
+          while(!pruned) {
+            if(!~followers.indexOf(target)) {
+              pruned = true;
+              
+              self
+                .twitter
+                .post('friendships/destroy.json')
+                .params({ id: target })
+                .end(callback);   
+            }
+          }
+      });
   });
+};
+
+var RTD2 = new Bot(config, 'github.com/');
+
+//get today's most popular tweet containing the tracked term, and tweet it
+RTD2.track(function(err, reply) {
+  if(err) console.log('error', err);
+  
+  var tweets = JSON.parse(reply).results
+    , popular = RTD2.getPopular(tweets);
+  
+  RTD2
+    .twitter
+    .post('statuses/update.json')
+    .params({ status: popular })
+    .end(function(err, reply) {
+      if(err) console.log('error:', err);
+      console.log('tweeted:', JSON.parse(reply).text);
+    })
+});
+
+RTD2.mingle(function(err, reply) {
+  if(err) console.log('error', err);
+  console.log('mingle: followed @%s', JSON.parse(reply).screen_name);
+});
+
+RTD2.prune(function(err, reply) {
+  if(err) console.log('error', err);
+  console.log('prune: unfollowed @%s', JSON.parse(reply).screen_name);
 });
