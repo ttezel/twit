f4bc50c76a6d1e58365917e2459771002384cbb6
Tolga Tezel <tolgatezel11@gmail.com>
2011-12-02 01:54:40 -0500

    api test

--- tests/api.js
+++ tests/api.js
@@ -4,13 +4,18 @@ var Twitter = require('../lib/twitter')
 
 var twitter = new Twitter(config);
 
-var bananahose = twitter.Stream.get('statuses/filter.json')
+var hose = twitter.Stream.get('statuses/filter.json')
                         .params({ track: 'banana' })
                         .persist();
 
 hose.on('data', function(data) {
-  console.log('data', data);
-})
+  try {
+    var tweets = JSON.parse(data);
+    console.log(tweets);
+  } catch (err) {
+    console.log('non-parsed data:', data); 
+  }
+});
 
 hose.on('error', function(err) {
   console.log('error:', err);
