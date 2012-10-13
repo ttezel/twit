86bb5784f93ff7066c7c6a06fc92b144ed372770
Tolga Tezel <tolgatezel11@gmail.com>
2012-10-13 03:54:14 -0400

    add moar rest api tests

--- tests/rest.js
+++ tests/rest.js
@@ -1,8 +1,8 @@
-var Twitter = require('../lib/twitter')
+var assert = require('assert')
+  , Twit = require('../lib/twitter')
   , config = require('../config')
-  , should = require('should')
 
-var twit = new Twitter(config);
+var twit = new Twit(config)
 
 describe('REST API', function () {
   it('POST `account/update_profile`', function (done) {
@@ -14,27 +14,52 @@ describe('REST API', function () {
   })
   it('GET `statuses/home_timeline`', function (done) {
     twit.get('statuses/home_timeline', function (err, reply) {
-      check(err, reply);
-      done();
-    });
-  });
+      check(err, reply)
+      done()
+    })
+  })
+  it('GET `statuses/mentions_timeline`', function (done) {
+    twit.get('statuses/mentions_timeline', function (err, reply) {
+      check(err, reply)
+      done()
+    })
+  })
+  it('GET `statuses/user_timeline`', function (done) {
+    twit.get('statuses/user_timeline', function (err, reply) {
+      check(err, reply)
+      done()
+    })
+  })
   it('GET `search/tweets` { q: "grape", since_id: 12345 }', function (done) {
-    var params = { q: 'grape', since_id: 12345 };
+    var params = { q: 'grape', since_id: 12345 }
     twit.get('search/tweets', params, function (err, reply) {
-      check(err, reply);
-      done();
-    });
-  });
+      check(err, reply)
+      done()
+    })
+  })
+  it('GET `search/tweets` { q: "banana", since: "2011-11-11" }', function (done) {
+    var params = { q: 'banana', since: '2011-11-11' }
+    twit.get('search/tweets', params, function (err, reply) {
+      check(err, reply)
+      done()
+    })
+  })
+  it('GET `direct_messages`', function (done) {
+    twit.get('direct_messages', function (err, reply) {
+      check(err, reply)
+      done()
+    })
+  })
   it('GET `followers/ids`', function (done) {
-      twit.get('followers/ids', function (err, reply) {
-        check(err, reply);
-        done();
-      });
-  });
-});
+    twit.get('followers/ids', function (err, reply) {
+      check(err, reply)
+      done()
+    })
+  })
+})
 
 function check (err, reply) {
-  should.not.exist(err);
-  reply.should.be.a('object');
-  (typeof reply === 'object' || Array.isArray(reply)).should.be.true;
-};
+  assert.equal(err, null)
+  assert.equal(typeof reply, 'object')
+  assert.ok(typeof reply === 'object' || Array.isArray(reply))
+}
