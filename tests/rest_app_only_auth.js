fb3eca8093166322a3278a429e60ae17de46acf1
Tolga Tezel <tolgatezel11@gmail.com>
2015-06-29 01:39:02 -0700

    Add some tests for REST API usage with app-only auth.

--- tests/rest_app_only_auth.js
+++ tests/rest_app_only_auth.js
@@ -1,23 +1,41 @@
+var assert = require('assert')
 
+var config1 = require('../config1');
+var Twit = require('../lib/twitter');
+var checkReply = require('./rest').checkReply;
+var checkResponse = require('./rest').checkResponse;
 
-describe('REST API app-only-auth', function () {
-  it('GET `account/verify_credentials`', function (done) {
-    twit.get('account/verify_credentials', function (err, reply, response) {
-      checkReply(err, reply)
-      assert.notEqual(reply.followers_count, undefined)
-      assert.notEqual(reply.friends_count, undefined)
-      assert.ok(reply.id_str)
+describe('REST API using app-only auth', function () {
+  var twit = null
+  before(function () {
+    var config = {
+      consumer_key: config1.consumer_key,
+      consumer_secret: config1.consumer_secret,
+      app_only_auth: true,
+    }
+    twit = new Twit(config)
+  })
 
+  it('GET `application/rate_limit_status`', function (done) {
+    twit.get('application/rate_limit_status', function (err, body, response) {
+      checkReply(err, body)
       checkResponse(response)
+      assert(body.rate_limit_context)
+      done()
+    })
+  })
 
-      assert(response.headers['x-rate-limit-limit'])
-
+  it('GET `application/rate_limit_status with specific resource`', function (done) {
+    var params = { resources: [ 'users', 'search' ]}
+    twit.get('application/rate_limit_status', params, function (err, body, response) {
+      checkReply(err, body)
+      checkResponse(response)
+      assert(body.rate_limit_context)
+      assert(body.resources.users)
+      assert(body.resources.search)
+      assert.equal(Object.keys(body.resources).length, 2)
       done()
     })
   })
 })
-
-before(function () {
-    twit = new Twit(config1);
-  })
 
